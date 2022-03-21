973f84d363e25f812d4e4616e0c2458d966728e9
bryan <bigavilanezb00@elpuig.xeill.net>
2022-03-22 00:44:13 +0100

    b

--- app/src/main/java/com/example/myanimelist/MainActivity.java
+++ app/src/main/java/com/example/myanimelist/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.myanimelist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.myanimelist.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+
+        NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding.navigationBottom, navController);
     }
 }
